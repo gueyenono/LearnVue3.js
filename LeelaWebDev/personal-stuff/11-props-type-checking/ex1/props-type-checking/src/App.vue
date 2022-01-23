21854f82a9f78eec3d57650dd3424268412cc6cc
gueyenono <nonoghislain@gmail.com>
2022-01-23 00:22:59 -0600

    I'm about to replace v-model with r everywhere in 11-props-type-checking/ex1

---
+++ LeelaWebDev/personal-stuff/11-props-type-checking/ex1/props-type-checking/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <form>
+    <label for="input-name">Name: </label>
+    <input v-model="name" type="text" id="input-name" placeholder="Your name">
+    {{ name }}
+    <br/><br/>
+    <label for="input-city">City: </label>
+    <input v-model="city" type="text" id="input-city" placeholder="Your city">
+    <br/><br/>
+    <label for="input-age">Age: </label>
+    <input v-model="age" type="number" id="input-age" placeholder="Your age">
+    <br/><br/>
+    <label for="input-application-date">Date of application: </label>
+    <input v-model="date" type="date" id="input-application-date">
+    <br/><br/>
+    <label for="input-married">Married: </label>
+    <select v-model="married" id="input-married">
+      <option value="true">Yes</option>
+      <option value="false">No</option>
+    </select>
+    <br/><br/>
+    <button
+      v-on:click.prevent="displayBio"
+    >Submit</button>
+  </form>
+  <hr/>
+  <BioDisplay
+    v-bind:isDisplayed="isDisplayed"
+    v-bind:name="name"
+    v-bind:city="city"
+    v-bind:age="age"
+    v-bind:date="date"
+    v-bind:married="married"
+  />
+</template>
+
+<script>
+import BioDisplay from './components/BioDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BioDisplay
+  },
+  data() {
+    return {
+      name: '',
+      city: '',
+      age: 0,
+      date: '',
+      married: '',
+      isDisplayed: false,
+    }
+  },
+  methods: {
+    displayBio() {
+      if(this.name && this.city && this.age !== 0 && !!this.date && this.married){
+        this.isDisplayed = true
+      } else {
+        alert('You need to fill out all your bio information.')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ LeelaWebDev/personal-stuff/11-props-type-checking/ex1/props-type-checking/src/components/BioDisplay.vue
@@ -0,0 +1,46 @@
+// Template --------------------------------------------------------------------
+<template>
+  <div
+    v-if="isDisplayed"
+  >
+    <p>Your name is <span>{{name}}</span>.</p>
+    <p>You live in <span>{{city}}</span>.</p>
+    <p>You are <span>{{age}}</span> years old.</p>
+    <p>You are <span v-if="married === 'false'">not</span> married.</p>
+    <p>It's been <span>not</span> days since you applied.</p>
+  </div>
+</template>
+
+
+// Script --------------------------------------------------------------------
+<script>
+export default {
+  props: {
+    isDisplayed: {
+      type: Boolean,
+      required: true
+    },
+    name: String,
+    city: String,
+    age: {
+      type: Number,
+      required: true,
+      validator: function(value) {
+        return value >= 0
+      }
+    },
+    // date: {
+    //   type: Date,
+    //   required: true,
+    //   default: new Date(Date.now())
+    // },
+    married: String,
+  },
+}
+</script>
+
+
+// Style --------------------------------------------------------------------
+<style>
+
+</style>
