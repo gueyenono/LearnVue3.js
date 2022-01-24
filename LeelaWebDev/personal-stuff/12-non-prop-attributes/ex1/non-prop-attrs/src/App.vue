4add0992f04679268d79f6bff7b7d14000706b99
gueyenono <nonoghislain@gmail.com>
2022-01-24 01:53:11 -0600

    minor

---
+++ LeelaWebDev/personal-stuff/12-non-prop-attributes/ex1/non-prop-attrs/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Comp
+    class="colored-grey colored-blue"
+  />
+</template>
+
+<script>
+import Comp from './components/Comp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comp
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ LeelaWebDev/personal-stuff/12-non-prop-attributes/ex1/non-prop-attrs/src/components/Comp.vue
@@ -0,0 +1,40 @@
+// Template --------------------------------------------------------------------
+<template>
+<div class="box">
+  <div class="box">
+    <div v-bind="$attrs" class="box">
+
+    </div>
+  </div>
+</div>
+</template>
+
+
+
+// Script ----------------------------------------------------------------------
+<script>
+export default {
+  inheritAttrs: false,
+  mounted() {
+    console.log(this.$attrs.class);
+  }
+}
+</script>
+
+
+
+// Style -----------------------------------------------------------------------
+<style scoped>
+.box {
+  border: 1px solid black;
+  padding: 1rem;
+}
+
+.colored-grey {
+  background-color: grey;
+}
+
+/* .colored-blue {
+  background-color: blue;
+} */
+</style>
